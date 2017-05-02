43d23b6614d3b345970f98917840d5c86bf52a4e
eric-ward <eric.ward@noaa.gov>
2017-05-02 14:55:19 -0700

    include default Limits as dataset for package

---
+++ R/data_defaultLimits.R
@@ -0,0 +1,14 @@
+#' Haul characteristics for 1756 hauls.
+#'
+#' An example dataset for the nwfscDeltaGLM package.
+#'
+#' @format A data frame with 1756 rows and 7 variables:
+#' \describe{
+#'   \item{STRATA}{strata name}
+#'   \item{NLat}{northern boundary, in decimal degrees}
+#'   \item{SLat}{southern boundary, in decimal degrees}
+#'   \item{MinDepth}{minimum depth, in meters}
+#'   \item{MaxDepth}{maximum depth, in meters}
+#'   ...
+#' }
+"defaultLimits"
